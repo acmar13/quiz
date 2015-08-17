73d39540eb11e0ad4165b64e2d7f3b3db926ca79
unknown <marvin.ancheta@4100203407-0554.digestyc.gob.sv>
2015-08-17 13:21:53 -0600

    identificación de versión

--- models/models.js
+++ models/models.js
@@ -1,12 +1,32 @@
 var path = require('path');
 
+//Postgres DATABASE_URL=postgres://user:password@host:port/database
+//SQLite DATABASE_URL=sqlite://:@:/
+var url     = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user    = (url[2]||null);
+var pwd     = (url[3]||null);
+var protocol= (url[1]||null);
+var dialect = (url[1]||null);
+var port    = (url[5]||null);
+var host    = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
+
 //cargar modelo ORM
 var Sequelize = require('sequelize');
 
 //cargar DB SQLite
-var sequelize = new Sequelize(null,null,null,
-                              {dialect: "sqlite",storage: "quiz.sqlite"});
-                    );
+
+var sequelize = new Sequelize(DB_name,user,pwd,
+    { dialect: protocol,
+      protocol:protocol,
+      port:     port,
+      host:     host,
+     storage:   storage, //solo SQLite (.env)
+     omitNull:  true    // solo postgres
+    }
+);
 
 //Importar la definición de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
@@ -19,9 +39,9 @@ sequelize.sync().success(function(){
         if(count==0){
             Quiz.create({ pregunta:'Capital de Italia',
                           respuesta:'Roma'
-                        });
+                        })
             .success(function(){console.log('Base de Datos inicializada')});
         }
     })
     
-    });
+});
